4caa611e4ce8023226674ff22257514024946199
Alena107868 <pogodaewa.ale@yandex.ru>
2024-06-27 21:07:00 +0300

    complex

---
+++ complex.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Complex:
+    real: float
+    imag: float
+
+
+    def __str__(self):
+        if self.imag >= 0:
+            sign = "+"
+        else:
+            sign = "-"
+        return f'{self.real} {sign} {abs(self.imag)}i'
+
+
+if __name__ == '__main__':
+    c = Complex(3, - 5)
+    print(c)
+
